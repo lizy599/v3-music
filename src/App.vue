e968027785b72cad548598b46d634e02271a29da
lizy599 <473517924@qq.com>
2023-11-01 17:08:03 +0800

    首页和tab标签

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header></Header>
+  <Tab></Tab>
+</template>
+<script>
+import Header from '@/components/header/header.vue'
+import Tab from '@/components/tab/tab.vue'
+export default {
+  components: {
+    Header,
+    Tab
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
